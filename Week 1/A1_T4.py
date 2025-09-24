9c25eda5b886ce8c357fe800b6ae67f1eee574a0
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-09-24 11:23:00 +0300

    Update A1_T4.py

---
+++ Week 1/A1_T4.py
@@ -0,0 +1,9 @@
+num1 = 47
+num2 = 102
+sum = num1 + num2
+diff = num2 - num1
+product = sum * diff
+print(f"{num1} + {num2} = {sum}")
+print(f"{num2} - {num1} = {diff}")
+print(f"{sum} * {diff} = {product}")
+print(f"({num1} + {num2}) * ({num2} - {num1}) = {product}")      
