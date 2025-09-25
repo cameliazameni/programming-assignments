30addf2787052130372e6797249895a4bc2d7463
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-09-25 06:52:07 +0300

    Update A1_T5.py

---
+++ Week 1/A1_T5.py
@@ -0,0 +1,8 @@
+print("calculate the area of a wall.")
+feed = input("Enter the width in meters: ")
+width = int(feed)
+feed = input("Enter the height in meters: ")
+height = int(feed)
+print(f"Width is {width} m and height is {height} m.")
+area = width * height
+print(f"The wall will be {area} square meters.")
