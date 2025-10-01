a68597f6da6717eeea0fc210c3c9bdafd21b1ff5
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-10-01 21:28:23 +0300

    change

--- Week 2/cheatsheet2.py
+++ Week 2/cheatsheet2.py
@@ -4,3 +4,13 @@ if a % 2 == 0 :
     print("The number is even")
 else:
     print("The number is odd")
+    
+
+
+if temp > 80:
+        if temp > 90:
+            print("The CPU is toast. Grt your fire extinguisher.")
+    else:
+        print("Warning! Temprature is too high")
+    else:
+print("All good, go ahead.")
