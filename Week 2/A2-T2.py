5d0e5b4c4d3826e7ffd83377b921b74dd00a7b4c
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-11-27 08:31:58 +0200

    Add car brand and model input and output

---
+++ Week 2/A2-T2.py
@@ -0,0 +1,7 @@
+print("Program starting.")
+CarBrand = input("Insert car brand: ")
+CarModel = input("Insert car model: ")
+print("Car brand is", f'"{CarBrand}"', sep=" ", end=" ")
+print("and the model is", f'"{CarModel}".', sep=" ")
+
+print("Program ending.")
