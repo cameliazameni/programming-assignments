fb3da367153367016d3ce072bc010a89888fc378
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-11-27 08:31:25 +0200

    Add multiplication program for two floating point numbers

---
+++ Week 2/A2-T1.py
@@ -0,0 +1,10 @@
+
+
+print("Program starting.")
+name = input("What is your name: ")
+Num1 = float(input("Enter a floating point number: "))
+Num2 = float(input("Enter second floating point number: "))
+Product = round(Num1 * Num2, 2)
+print(f"{name} you gave numbers {Num1} and {Num2}")
+print(f"Multiplying first and second number will result in {Product}")
+print("Program ending.")
