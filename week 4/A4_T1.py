b285c2dcfbeb3a1470f6b089097d084c730d41e0
cameliazameni <camelia.zameni.tahan@student.lab.fi>
2025-12-01 01:15:13 +0200

    Update A4_T1.py

---
+++ week 4/A4_T1.py
@@ -0,0 +1,6 @@
+print("Program starting.")
+start = int(input("Insert starting value: "))
+stop = int(input("Insert ending value: "))
+for i in range(start, stop + 1):
+    print(i)
+print("Program ending.")
